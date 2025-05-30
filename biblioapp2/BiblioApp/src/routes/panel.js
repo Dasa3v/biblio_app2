acf8e6d319cd99114a5635fb2108897faac7540a
Dasa3v <dasaev.cor@gmail.com>
2025-05-30 10:39:43 -0500

    Delete biblioapp2 directory

--- biblioapp2/BiblioApp/src/routes/panel.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const panelController = require('../controllers/panelController');
-
-router.get('/', panelController.index);
-
-module.exports = router;
